f4ebd511eb6bd20ba414ec4a5ad8f4aa7528e884
GeorgeMalandris <georgemalandris@gmail.com>
2021-01-31 18:54:02 +0200

    Added assignment 1

---
+++ JS/JQButtons.js
@@ -0,0 +1,26 @@
+﻿$("#cancelBtn").click(function (e) {
+    e.preventDefault();
+    window.history.back();
+});
+
+$("input[type=submit]").click(function (e) {
+    e.preventDefault();
+    let formEle = e.target.closest("form");
+    let checker = formEle.checkValidity();
+    formEle.reportValidity();
+    if (checker) {
+        window.history.back();
+    }
+});
+
+$(".deleteBtn").click(function (e) {
+    let row = e.target.closest("tr");
+    $("#deleteModal").modal("show");
+    $("#approveDelete").click(function (e) {
+        $("#deleteModal").modal("hide");
+        row.remove();
+    });
+});
+
+console.log("ok");
+    
---
+++ JS/JQDateConstrains.js
@@ -0,0 +1,25 @@
+﻿function dateConstrain(minYearsOffset, maxYearsOffset) {
+    if (isNaN(minYearsOffset || minYearsOffset < 0)) {
+        minYears = 0;
+    }
+    if (isNaN(maxYearsOffset || maxYearsOffset < 0)) {
+        maxYears = 0;
+    }
+    let a = new Date();
+    let minYear = a.getFullYear() + minYearsOffset;
+    let maxYear = a.getFullYear() + maxYearsOffset;
+    let month = a.getMonth() + 1;
+    if (month < 10) {
+        month = `0${month}`
+    }
+    let day = a.getDate();
+    if (day < 10) {
+        day = `0${day}`
+    }
+    let dates = $("input[type = 'date']");
+
+    for (var i of dates) {
+        i.min = `${minYear}-${month}-${day}`;
+        i.max = `${maxYear}-${month}-${day}`;
+    }
+}
